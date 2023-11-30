486fd74509db5f10fd501916538fb9df5db83c46
ssking786 <139364193+ssking786@users.noreply.github.com>
2023-11-30 21:33:09 +0500

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,1 +1,16 @@
-import ssktest_enc
+import os,platform
+print(f'\x1b[1;92m[âˆš] PLEASE WAIT CHECKING UPDATE...')
+os.system('git pull')
+os.system("clear")
+print(f'\x1b[1;92m[âˆš] MISSING MODULED INSTALING...')
+os.system("pkg install espeak -y")
+os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+os.system("clear")
+print('\033[1;32m [â€¢] JOIN RANA WhATSAP GROUP')
+os.system('xdg-open https://chat.whatsapp.com/GWTPaJVAy1gDHIDgHEw0Ht')
+os.system("clear")
+rana=platform.architecture()[0]
+if rana=="32bit":
+    __import__("TEST")
+elif rana=="64bit":
+    __import__("TEST")
