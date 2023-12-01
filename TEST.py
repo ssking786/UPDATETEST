00a141b87c3ce840515fa42bc2188ff201831392
ssking786 <139364193+ssking786@users.noreply.github.com>
2023-12-01 18:12:11 +0500

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,7 +1,4 @@
-import os, sys, platform
- 
-os.system('test_enc')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf aws1.so aws32.so')
+import os,sys,platform
+
+import test_enc
+import data_enc
